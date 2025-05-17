812e4032dd3b53bfacdcb3ddc77febb991ffe5d3
jsm020 <javohirxikmatillaev@gmail.com>
2025-05-17 14:54:07 +0300

    Update URL routing to use botadmin URLs and remove unused cache files

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from botadmin.api import router as botadmin_router
+from botadmin.urls import router as botadmin_router
 
 urlpatterns = [
     path("admin/", admin.site.urls),
